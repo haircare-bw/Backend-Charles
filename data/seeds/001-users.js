b153af93ae7de4803c2a1c0892a5c2c10d13f086
charles-rogers <c.a.r0g3r5@gmail.com>
2019-06-25 12:47:38 -0500

    still working on heroku seeding

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,8 +1,13 @@
 const bcrypt = require('bcryptjs');
 
 exports.seed = function(knex, Promise) {
-  return knex('users').insert([
-    { username: 'kedasha', password: bcrypt.hashSync('kerr', 10), type: 'user' }, // 1
-  ]);
+  return knex('users')
+  .truncate()
+    .then(function () {
+      // Inserts seed entries
+      return knex('users').insert([
+        { username: 'kedasha', password: bcrypt.hashSync('kerr', 10), type: 'user' }, // 1
+      ]);
+  });
 };
 
