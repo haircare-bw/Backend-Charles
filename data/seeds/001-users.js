d453896b8e6f2bea5bb98b45efbf1bcc79b7320e
charles-rogers <c.a.r0g3r5@gmail.com>
2019-06-25 14:46:31 -0500

    still working on seeds

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -1,13 +1,9 @@
 const bcrypt = require('bcryptjs');
 
 exports.seed = function(knex, Promise) {
-  return knex('users')
-  .truncate()
-    .then(function () {
-      // Inserts seed entries
-      return knex('users').insert([
-        { username: 'kedasha', password: bcrypt.hashSync('kerr', 10), type: 'user' }, // 1
-      ]);
-  });
+ // Inserts seed entries
+    return knex('users').insert([
+      { username: 'kedasha', password: bcrypt.hashSync('kerr', 10), type: 'user' }
+    ]);
 };
 
